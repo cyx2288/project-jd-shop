ae6f15b9d919f65a5e343fda3e306ba74a31a014
zhuangyi28 <596363901@qq.com>
2017-05-23 21:02:33 +0800

    嘉福京东商城首页静态页面
    版本：v0.0.1

---
+++ gulp/dev/ejs.dev.js
@@ -0,0 +1,116 @@
+/**模板引擎合并
+ * 开发*/
+
+var gulp = require('gulp'),
+
+    ejs = require('gulp-ejs'),//ejs模板
+
+    cheerio = require('gulp-cheerio'),//批量更换html中的引用
+
+    connect = require('gulp-connect'),//服务器
+
+    rename = require("gulp-rename");//重命名
+
+
+function devEjs() {
+
+    gulp.src('src/view/*/**.{ejs,html}')
+
+        .pipe(ejs())
+
+        //增加媒体查询，通用样式文件
+        .pipe(cheerio({
+
+                run:function ($) {
+
+                    if ($('head').children('link')) {
+
+                        $('head').children('link').remove();//重新引入全新css
+
+                        $('head').append(addHtml);
+
+                    }
+
+                    var addHtml = "";
+
+                    addHtml += "<link rel='stylesheet'  href='../../css/jdShopMain.css'/>\n ";//框架
+
+
+                    $('head').prepend(addHtml);
+
+                },
+
+            parserOptions: {
+                // Options here
+                decodeEntities: false
+            }
+
+            }
+        ))
+
+        //顺序增加脚本文件
+        .pipe(cheerio(function ($) {
+
+            var addJSHtml = '';//保存引用的业务脚本
+
+            var addJsRun = "<script src='../../js/jdShopMain.js'></script>\n";//运行的脚本
+
+            var addJsHtmlHead = "<script src='";
+
+            var addJSHtmlBottom = "'></script>\n";
+
+            $('script').each(function (index, ele) {
+
+                if ($(this).attr('src')) {
+
+                    var thisAttr=$(this).attr('src');
+
+                    if(thisAttr.indexOf('jdShopMain.min.js')>-1){
+
+                        thisAttr=thisAttr.replace('jdShopMain.min.js','jdShopMain.js')
+
+                    }
+
+                    addJSHtml += addJsHtmlHead + thisAttr + addJSHtmlBottom;
+
+                }
+
+                else {
+
+                    addJsRun += $(this).html() + '\n';
+
+                }
+
+            });
+
+            addJsRun += "\n</script>\n";
+
+
+            $('script').remove();
+
+            $('body').append(addJSHtml);
+
+            $('body').append(addJsRun);
+
+        }))
+
+        .pipe(rename({
+
+            extname: ".html"
+
+        }))
+
+        .pipe(gulp.dest('build/html'))//输出为html
+
+        //.pipe(notify({message: 'html task complete'}));
+
+        .pipe(connect.reload());
+
+    /*框架html导入*/
+    gulp.src("src/*.html")
+
+        .pipe(gulp.dest('build'));
+
+}
+
+module.exports = devEjs;
