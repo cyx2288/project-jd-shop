38bbaa9cea42e4900c097d51f6c6dc6177d847d2
zhuangyi28 <596363901@qq.com>
2017-09-06 10:28:28 +0800

    详情页看京东价&点穿问题修正0906

--- src/component/search_results/search_drop_down.js
+++ src/component/search_results/search_drop_down.js
@@ -3,15 +3,116 @@
  */
 var jfDropDown = function (details) {
 
+    if(!details){
+
+        details ={}
+
+    }
+
     this.details = details;
 
     var thisEle = document.getElementById(this.details.ele);
+
+
+    //var thisfatherEle = this.details.fatherId || 0;
+
+    var thishasScrollEle = this.details.scrollClassname || 0;
 
     thisEle.getElementsByClassName('jd_drop_down_bg')[0].addEventListener('click', clickEven.bind(this), false);
 
     function clickEven() {
 
         this.hide();
+
+    }
+
+    if(thishasScrollEle){
+
+        clickThought(thishasScrollEle);
+
+    }
+
+
+    function clickThought(thishasScrollEle) {
+
+
+        var thisScrollEle = thisEle.getElementsByClassName(thishasScrollEle)[0];
+
+
+
+        var startY, endY, distance;//开始距离、移动距离
+
+        thisScrollEle.addEventListener('touchstart', touchStartEle, false);
+
+        thisScrollEle.addEventListener('touchmove', reachEdge, false);
+
+
+        //thisScrollEle.addEventListener('touchmove', reachEdge, false);
+
+
+        function touchStartEle(e) {
+
+            //touchstart 获取位置startY
+
+            startY = e.touches[0].pageY;
+
+        }
+
+
+        function reachEdge(event) {
+
+            var _this = this;
+
+            var eleScrollHeight = _this.scrollTop;//获取滚动条的位置 206
+
+            var eleHeight = _this.scrollHeight;//元素实际高度 506
+
+            var containerHeight = _this.offsetHeight;//容器高度 300
+
+
+            //touchmove 获取位置 endY
+
+            endY = event.touches[0].pageY;
+
+            //两者之减的距离用来判断是向上活动还是向下滑动
+            distance = startY - endY;
+
+            //此时touchmove的值等于touchstart的值 循环
+            endY = startY;
+
+
+            //滚动条到达底部
+
+            if (Math.abs(parseFloat(eleHeight) - parseFloat(eleScrollHeight + containerHeight)) <= 2) {
+
+
+                //如果距离为正数 则向上滑动是 禁止浏览器事件
+
+                if (distance > 0) {
+
+                    event.preventDefault();
+
+
+                }
+
+
+            }
+
+            else if (Math.abs(parseFloat(eleScrollHeight)) == 0) {
+
+                //如果距离为负数 则向下滑动
+
+                if (distance < 0) {
+
+                    event.preventDefault();
+
+                }
+
+
+            }
+
+        }
+
 
     }
 
@@ -30,10 +131,6 @@ var jfDropDown = function (details) {
     }
 
 
-
-
-
-
     function addEvent(ele) {
 
         var allEvent=['touchstart','touchmove','touchend'];
@@ -50,11 +147,11 @@ var jfDropDown = function (details) {
 
         // window.event? window.event.cancelBubble = true : e.stopPropagation();
 
-        if(browser.os.iOS) {
+        //if(browser.os.iOS) {
 
             window.event ? window.event.returnValue = false : e.preventDefault();
 
-        }
+        //}
     }
 
 };
@@ -68,13 +165,15 @@ jfDropDown.prototype.show = function (details) {
 
     }
 
-    var thisScrollEle = this.details.thisScrollEle || 0;//含有滚动条元素的classname
+    //var thisScrollEle = this.details.thisScrollEle || 0;//含有滚动条元素的classname
 
 
+/*
     if(this.details.thisScrollEle){//如果有值 则执行
 
         clickThrough(thisScrollEle);
     }
+*/
 
 
     /* this.ban();*/
@@ -102,88 +201,7 @@ jfDropDown.prototype.show = function (details) {
 
     //解决弹框点击穿透问题-0831
 
-    function clickThrough(thisScrollEle) {
 
-        var _thisScrollEle = document.getElementsByClassName(thisScrollEle)[0];
-
-        var startY, endY, distance;//开始距离、移动距离
-
-        _thisScrollEle.addEventListener('touchstart', touchStartEle, false);
-
-        _thisScrollEle.addEventListener('touchmove', touchMoveDistance, false);
-
-        _thisScrollEle.addEventListener('touchmove', reachEdge, false);
-
-
-        function touchStartEle(e) {
-
-            //touchstart 获取位置startY
-
-            startY = e.touches[0].pageY;
-
-        }
-
-
-        function touchMoveDistance(e) {
-
-            //touchmove 获取位置 endY
-
-            endY = e.touches[0].pageY;
-
-            //两者之减的距离用来判断是向上活动还是向下滑动
-            distance = startY - endY;
-
-            //此时touchmove的值等于touchstart的值 循环
-            endY = startY;
-
-        }
-
-
-        function reachEdge(event) {
-
-            var _this = this;
-
-            var eleScrollHeight = _this.scrollTop;//获取滚动条的位置 206
-
-            var eleHeight = _this.scrollHeight;//元素实际高度 506
-
-            var containerHeight = _this.offsetHeight;//容器高度 300
-
-
-            //滚动条到达底部
-
-            if (Math.abs(parseFloat(eleHeight) - parseFloat(eleScrollHeight + containerHeight)) <= 2) {
-
-
-                //如果距离为正数 则向上滑动时候 禁止浏览器事件
-
-                if (distance > 0) {
-
-                    event.preventDefault();
-
-
-                }
-
-
-            }
-
-            else if (Math.abs(parseFloat(eleScrollHeight)) == 0) {
-
-                //如果距离为负数 则向下滑动 禁止浏览器事件
-
-                if (distance < 0) {
-
-                    event.preventDefault();
-
-                }
-
-
-            }
-
-        }
-
-
-    }
 
 
 
